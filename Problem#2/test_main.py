afe0be51cf715fbf49a0d2c68eb2f12b0a32b324
Mateusz_Ormianek <mattormianek@gmail.com>
2023-03-29 22:22:35 +0200

    Added better solution, still can't figure out better tests for that.

--- Problem#2/test_main.py
+++ Problem#2/test_main.py
@@ -2,9 +2,16 @@ import pytest
 
 from main import Solution
 
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 def test_use_case_proper_value_first() -> None:
-    list_1 = [2,4,3]
-    list_2 = [5,6,4]
+    list_1 = ListNode()
+    #list_1.val = (2,4,3)
+    list_2 = ListNode()
+    #list_2 = (5,6,4)
     output = Solution.addTwoNumbers(list_1,list_2)
     assert output == [7,0,8]
 
