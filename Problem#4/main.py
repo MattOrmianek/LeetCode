d5099aa0dbe75b139488ea300feffe523183a15e
Mateusz_Ormianek <mattormianek@gmail.com>
2023-04-02 23:00:59 +0200

    Changes

--- Problem#4/main.py
+++ Problem#4/main.py
@@ -15,12 +15,12 @@ class Solution(object):
         l, r = 0, len(nums1) - 1
         while True:
             i = (l + r) // 2
-            j = half - i -2
+            j = half - i - 2
 
-            nums1_left = nums1[i] if i >= 0 else float('-inf')
-            nums1_right = nums1[i+1] if i+1 < len(nums1) else float('inf')
-            nums2_left = nums2[j] if j >= 0 else float('-inf')
-            nums2_right = nums2[j+1] if j+1 < len(nums2) else float('inf')
+            nums1_left = float(nums1[i] if i >= 0 else float('-inf'))
+            nums1_right = float(nums1[i+1] if i+1 < len(nums1) else float('inf'))
+            nums2_left = float(nums2[j] if j >= 0 else float('-inf'))
+            nums2_right = float(nums2[j+1] if j+1 < len(nums2) else float('inf'))
 
             if nums1_left <= nums2_right and nums2_left <= nums1_right:
                 if total % 2 == 0:
