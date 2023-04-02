50762f2b79bca30205761aa871502306d96da57c
Mateusz_Ormianek <mattormianek@gmail.com>
2023-04-02 23:22:21 +0200

    Added files with instruction, to be done

---
+++ Problem#5/main.py
@@ -0,0 +1,8 @@
+# Given a string s, return the longest palindromic substring
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
---
+++ Problem#5/test_main.py
@@ -0,0 +1,6 @@
+import pytest
+
+from main import Solution
+
+def test_use_case_proper_value_first() -> None:
+    pass
